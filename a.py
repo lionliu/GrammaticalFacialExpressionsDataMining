3db16e54bd18306cc4b5f939d56ae5dae98bb7dd
Leao Liu Masur <llm2@cin.ufpe.br>
2019-10-24 22:30:46 -0300

    Filtered outliers with z-score in the pre-processing notebook

--- a.py
+++ a.py
@@ -1,13 +1,36 @@
 import numpy as np
+import pandas as pd
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from scipy import stats
+from support import getDFPointsAngles
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-a = np.array([6, 0])
-b = np.array([0, 0])
-c = np.array([0, 6])
+dataset = pd.read_csv("./dataset/a_affirmative_datapoints.csv", sep=' ')
+targets = pd.read_csv("./dataset/a_affirmative_targets.csv")
+dataset = pd.concat([dataset, targets], axis=1)
+dataset.drop([dataset.columns[0]], axis=1, inplace=True)
 
-ba = a - b
-bc = c - b
+dataset2 = getDFPointsAngles(dataset, targets)
+print(dataset2.describe())
+dataset2.plot(kind='box', subplots=True, layout=(
+    4, 5), sharex=False, sharey=False)
+plt.show()
 
-cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
-angle = np.arccos(cosine_angle)
+z = np.abs(stats.zscore(dataset2))
 
-print(angle)
+dataset2 = dataset2[(z < 2.5).all(axis=1)]
+
+dataset2.plot(kind='box', subplots=True, layout=(
+    4, 5), sharex=False, sharey=False)
+plt.show()
+
+X = dataset2.iloc[:, :-1]
+y = dataset2.iloc[:, -1]
+
+XScaled = MinMaxScaler().fit_transform(X)
+normDataset = pd.DataFrame(XScaled, columns=['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+                                             'd10', 'd11', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7'])
+normDataset['target'] = dataset['target']
+print(normDataset.describe())
+
