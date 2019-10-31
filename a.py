d4e146406257ce547b69d3c87638da1b6cd687e9
Leao Liu Masur <llm2@cin.ufpe.br>
2019-10-31 19:08:29 -0300

    Created pre processed .csv files

--- a.py
+++ a.py
@@ -6,31 +6,31 @@ from scipy import stats
 from support import getDFPointsAngles
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-dataset = pd.read_csv("./dataset/a_affirmative_datapoints.csv", sep=' ')
-targets = pd.read_csv("./dataset/a_affirmative_targets.csv")
-dataset = pd.concat([dataset, targets], axis=1)
-dataset.drop([dataset.columns[0]], axis=1, inplace=True)
 
-dataset2 = getDFPointsAngles(dataset, targets)
-print(dataset2.describe())
-dataset2.plot(kind='box', subplots=True, layout=(
-    4, 5), sharex=False, sharey=False)
-plt.show()
+def genCSV(name):
+    dataset = pd.read_csv("./dataset/" + name +"_datapoints.csv", sep=' ')
+    targets = pd.read_csv("./dataset/"+ name + "_targets.csv")
+    dataset = pd.concat([dataset, targets], axis=1)
+    dataset.drop([dataset.columns[0]], axis=1, inplace=True)
 
-z = np.abs(stats.zscore(dataset2))
+    dataset2 = getDFPointsAngles(dataset, targets)
 
-dataset2 = dataset2[(z < 2.5).all(axis=1)]
+    z = np.abs(stats.zscore(dataset2))
+    dataset2 = dataset2[(z < 2.5).all(axis=1)]
 
-dataset2.plot(kind='box', subplots=True, layout=(
-    4, 5), sharex=False, sharey=False)
-plt.show()
+    X = dataset2.iloc[:, :-1]
+    y = dataset2.iloc[:, -1]
 
-X = dataset2.iloc[:, :-1]
-y = dataset2.iloc[:, -1]
+    XScaled = MinMaxScaler().fit_transform(X)
+    normDataset = pd.DataFrame(XScaled, columns=['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+                                                'd10', 'd11', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7'])
+    normDataset['target'] = dataset['target']
 
-XScaled = MinMaxScaler().fit_transform(X)
-normDataset = pd.DataFrame(XScaled, columns=['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
-                                             'd10', 'd11', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7'])
-normDataset['target'] = dataset['target']
-print(normDataset.describe())
+    normDataset.to_csv("./PreprocessedDataset/" + name + ".csv", index=False, sep=" ")
 
+
+names = ["a_affirmative", "a_conditional", "a_doubt_question", "a_emphasis", "a_negative", "a_relative", "a_topics", "a_wh_question", "a_yn_question",
+         "b_affirmative", "b_conditional", "b_doubt_question", "b_emphasis", "b_negative", "b_relative", "b_topics", "b_wh_question", "b_yn_question", ]
+
+for i in names:
+    genCSV(i)
