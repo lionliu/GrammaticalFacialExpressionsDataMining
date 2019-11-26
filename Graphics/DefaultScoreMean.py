7da7f0e2ba27ffb9d51fe282e110cea16ceb95a3
Leao Liu Masur <llm2@cin.ufpe.br>
2019-11-26 09:58:24 -0300

    Created plots for the mean scores

---
+++ Graphics/DefaultScoreMean.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from statistics import mean
+
+
+treeDefault = [0.92,0.97,0.95,0.96,0.92,0.97,0.96,0.94,0.95,0.91,0.89,0.93,0.94,0.94,0.96,0.93,0.94,0.92,]
+
+knnDefault = [0.85,0.95,0.92,0.88,0.89,0.94,0.89,0.91,0.93,0.83,0.85,0.83,0.84,0.83,0.81,0.87,0.92,0.81,]
+
+mlpDefault = [0.61,0.84,0.87,0.78,0.49,0.76,0.76,0.46,0.76,0.55,0.77,0.71,0.63,0.68,0.71,0.83,0.79,0.61,]
+
+forestDefault = [0.88,0.90,0.91,0.91,0.84,0.90,0.91,0.85,0.91,0.82,0.79,0.84,0.87,0.86,0.91,0.84,0.79,0.80,]
+
+baggingDefault = [0.72,0.87,0.85,0.75,0.71,0.85,0.79,0.68,0.84,0.67,0.69,0.752,0.761,0.76,0.69,0.84,0.86,0.74,]
+
+votingDefault = [0.59,0.77,0.81,0.53,0.65,0.81,0.66,0.58,0.84,0.59,0.67,0.73,0.76,0.71,0.66,0.86,0.77,]
+
+scoresDefault = [treeDefault, knnDefault, mlpDefault, forestDefault, baggingDefault, votingDefault]
+scoresMean = []
+
+for s in scoresDefault:
+    scoresMean.append(float(format(mean(s), '.3f')))
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        plt.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+x = np.arange(6)
+
+
+plt.figure()
+plt.title("Default dataset mean scores")
+plt.xlabel('Classifiers')
+plt.ylabel('Mean Scores')
+rect = plt.bar(x, scoresMean)
+autolabel(rect)
+plt.xticks(x, ('tree', 'KNN', 'MLP', 'RF', 'Bagging', 'Voting'))
+plt.show()
---
+++ Graphics/PreProprocessedScoreMean.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from statistics import mean
+
+
+treePrep = [0.75,0.79,0.70,0.79,0.71,0.78,0.80,0.70,0.73,0.83,0.83,0.84,0.82,0.84,0.85,0.85,0.82,0.75,]
+
+knnPrep = [0.84,0.87,0.83,0.83,0.76,0.87,0.85,0.77,0.83,0.88,0.93,0.93,0.92,0.91,0.93,0.91,0.90,0.92,]
+
+mlpPrep = [0.68,0.72,0.63,0.79,0.64,0.75,0.78,0.65,0.61,0.67,0.7,0.66,0.65,0.66,0.78,0.78,0.80,0.73,]
+
+forestPrep = [0.75,0.83,0.77,0.86,0.74,0.83,0.85,0.77,0.77,0.77,0.87,0.89,0.87,0.88,0.89,0.90,0.84,0.75,]
+
+baggingPrep = [0.72,0.71,0.69,0.82,0.64,0.73,0.79,0.58,0.64,0.67,0.7,0.66,0.67,0.66,0.81,0.78,0.77,0.755,]
+
+votingPrep = [0.72,0.71,0.57,0.62,0.66,0.66,0.65,0.60,0.65,0.73,0.75,0.75,0.70,0.74,0.79,0.70,0.82,]
+
+scoresPrep = [treePrep, knnPrep, mlpPrep, forestPrep, baggingPrep, votingPrep]
+scoresMean = []
+
+for s in scoresPrep:
+    scoresMean.append(float(format(mean(s), '.3f')))
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        plt.annotate('{}'.format(height, '.3f'),
+                     xy=(rect.get_x() + rect.get_width() / 2, height),
+                     xytext=(0, 3),  # 3 points vertical offset
+                     textcoords="offset points",
+                     ha='center', va='bottom')
+                     
+x = np.arange(6)
+
+
+plt.figure()
+plt.xlabel('Classifiers')
+plt.ylabel('Mean Scores') 
+plt.title("Preprocessed dataset mean scores")
+rect = plt.bar(x, scoresMean)
+autolabel(rect)
+plt.bar(x, scoresMean)
+plt.xticks(x, ('tree', 'KNN', 'MLP', 'RF', 'Bagging', 'Voting'))
+plt.show()
+
